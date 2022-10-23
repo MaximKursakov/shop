8857bcb4050723c7e19e3394b75f9639c15db60a
maxim <maximcarlos@gmx.de>
2022-10-23 11:43:29 +0200

    items can be removed from basket

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,18 @@
 import { useLocation } from "react-router-dom"
 
 export function Cart ({Basket, setBasket}) {
+    function removeItem(el) {
+        setBasket(Basket.filter((item) => {
+            return item !== el
+        }));
+    }
     return(
         <div>
             {Basket.map((item) => (
-                <p key={item.id}>{item.title}</p>
+                <div key={item.id}>
+                    <p>{item.title}</p>
+                    <button onClick={() => removeItem(item)}>remove</button>
+                </div>
             ))}
         </div>
     )
