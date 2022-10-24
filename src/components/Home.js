525b8f97860343035da42cfbd286124b94125cf4
maxim <maximcarlos@gmx.de>
2022-10-24 17:34:10 +0200

    hero basic

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,12 @@ export function Home () {
     return(
         <div className="home">
             <div className="hero">
-                <h1>Some Title Text</h1>
-                <Link to="/Chairs">Shop now</Link>
+                <img src="/images/ch23.png" className="hero-img" alt="ch23 chair"></img>
+                <div>
+                    <h1>Some Title Text</h1>
+                    <Link to="/Chairs">Shop now</Link>
+                </div>
+                <img src="images/ch26.png" className="hero-img" alt="ch26 chair"></img>
             </div>
         </div>
     )
