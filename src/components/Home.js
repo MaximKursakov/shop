83d6f1158bee627b6f4821845eccbc0fd5d47192
maxim <maximcarlos@gmx.de>
2022-10-24 18:03:32 +0200

    hero styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ export function Home () {
         <div className="home">
             <div className="hero">
                 <img src="/images/ch23.png" className="hero-img" alt="ch23 chair"></img>
-                <div>
-                    <h1>Some Title Text</h1>
-                    <Link to="/Chairs">Shop now</Link>
+                <div className="hero-mid">
+                    <h2>COLLECTION</h2>
+                    <h1>NAME</h1>
+                    <p>Some Title Text that describes the company</p>
+                    <Link to="/Chairs"><button className="shop-link">Shop now</button></Link>
                 </div>
                 <img src="images/ch26.png" className="hero-img" alt="ch26 chair"></img>
             </div>
