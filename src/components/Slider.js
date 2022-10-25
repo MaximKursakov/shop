3ece868091aef4179f02893e7d3cb810fcc49b79
maxim <maximcarlos@gmx.de>
2022-10-25 16:44:37 +0200

    carousel works (without children fading out)

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,20 +1,35 @@
 import { motion, useMotionValue, useTransform } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
 
 export function Slider () {
     const x = useMotionValue(0)
+    
     const scale = useTransform(x, [500, 0, -500], [0.5, 1, 0.5])
     const opacity = useTransform(x, [1000, 500, 0, -500, -1000], [0, 0, 1, 0, 0])
+    const carousel = useRef()
+    const [width, setWidth] = useState(0)
+    useEffect(() => {
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+        console.log(width)
+    }, [])
+    
+    
     return(
-        <div>
-            <motion.button className="slider" 
-                style={{x:x, scale:scale, opacity: opacity }} 
-                transition={{}} drag="x" 
-                dragConstraints={{top: 0, bottom: 0}} 
-                dragElastic={0}>
-                <motion.div>TEST 1</motion.div>
-                <motion.div>TEST 2</motion.div>
-                <motion.div>TEST 3</motion.div>
-            </motion.button>
+        <div >
+            <motion.div ref={carousel}>
+                <motion.button className="slider"
+                    
+                    drag="x"
+                    dragConstraints={{right: 0, left: -width}} 
+                    >
+                    <motion.div >TEST 1</motion.div>
+                    <motion.div >TEST 2</motion.div>
+                    <motion.div >TEST 3</motion.div>
+                    <motion.div >TEST 4</motion.div>
+                    <motion.div >TEST 5</motion.div>
+                    <motion.div >TEST 6</motion.div>
+                </motion.button>
+            </motion.div>
         </div>
     )
 }
