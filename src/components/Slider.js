5bb869a913711c9a73dc1ebd78ba12d0a403ab32
maxim <maximcarlos@gmx.de>
2022-10-25 19:06:45 +0200

    slider movable from to sides

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,18 +5,31 @@ export function Slider () {
     const testInput = [{title: "test1", index: 1}, {title: "test1", index: 2}, {title: "test1", index: 3}, {title: "test1", index: 4}]
     const [position, setPosition] = useState(0)
 
+    function slideRight() {
+        if (position <= testInput.length) {
+            setPosition(position + 1)
+            console.log("yes")
+        }
+    }
 
-    
+    function slideLeft() {
+        if (position >= 0) {
+            setPosition(position - 1)
+        }
+    }
     
     return(
         <div >
+            <button onClick={slideLeft}> left </button>
+            <button onClick={slideRight}> right </button>
             <motion.div className="slider">
                 {testInput.map((item) => (
                     <motion.div 
                     key={item.index} 
                     className="container"
                     animate={{
-                        left: item.index * 500
+                        left: (item.index - position) * 500
+
                     }}>
                         <p>{item.title}</p>
                     </motion.div>
