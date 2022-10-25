313b16424df8d66da7923f66dec419fe54779ab9
maxim <maximcarlos@gmx.de>
2022-10-25 13:58:05 +0200

    silder items vanish on drag

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,19 @@
-import { motion } from "framer-motion"
+import { motion, useMotionValue, useTransform } from "framer-motion"
 
 export function Slider () {
+    const x = useMotionValue(0)
+    const scale = useTransform(x, [500, 0, -500], [0.5, 1, 0.5])
+    const opacity = useTransform(x, [1000, 500, 0, -500, -1000], [0, 0, 1, 0, 0])
     return(
         <div>
-            <motion.button className="slider" drag dragConstraints={{top: 0, bottom: 0}} dragElastic={0}>
-                TEST
+            <motion.button className="slider" 
+                style={{x:x, scale:scale, opacity: opacity }} 
+                transition={{}} drag="x" 
+                dragConstraints={{top: 0, bottom: 0}} 
+                dragElastic={0}>
+                <motion.div>TEST 1</motion.div>
+                <motion.div>TEST 2</motion.div>
+                <motion.div>TEST 3</motion.div>
             </motion.button>
         </div>
     )
