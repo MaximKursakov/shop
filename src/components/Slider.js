eba8b85e47fe21dd7536d26553d4c37c8b87169c
maxim <maximcarlos@gmx.de>
2022-10-26 10:46:24 +0200

    responsivness improved and bug fixes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,24 +3,27 @@ import { useEffect, useRef, useState } from "react"
 
 export function Slider ({chairInfo}) {
 
-    const [position, setPosition] = useState(1)
+    const [position, setPosition] = useState(2)
     const [containerWidth, setContainerWidth] = useState(0)
+    // const [featuredChairs, setFeaturedChairs] = useState(chairInfo.filter((chair) => chair.featured))
+
+    const featuredChairs = chairInfo.filter((chair) => chair.featured)
+    
 
     const container = useRef()
     useEffect(() => {
         
         setContainerWidth(container.current.offsetWidth)
-        console.log(containerWidth)
     })
 
     function slideRight() {
-        if (position < chairInfo.length -1) {
+        if (position < featuredChairs.length ) {
             setPosition(position + 1)
         }
     }
 
     function slideLeft() {
-        if (position - 1 >= 0) {
+        if (position-1  > 0) {
             setPosition(position - 1)
         }
     }
@@ -30,27 +33,27 @@ export function Slider ({chairInfo}) {
             <button onClick={slideLeft}> left </button>
             <button onClick={slideRight}> right </button>
             <motion.div ref={container} className="slider">
-                {chairInfo.map((item) => (
+                {featuredChairs.map((item) => {
+                    
+                    return(
                     <div>
-                        {item.featured 
-                        &&<motion.div 
+                        <motion.div 
                         key={item.id} 
                         className="container"
                         animate={{
-                            left: (item.id - position) * (containerWidth / 2),
+                            left: (item.id - position) * (containerWidth / 1.3),
                             scale: position === item.id  ? 1.2 : 0.8,
                             opacity: position === item.id  ? 1 : 0.5,
                         }}>
                         <img className="featured-img"src={`images/${item.title}.png`} alt="chair"></img>
-                            {position === item.id - 1 
+                            {position === item.id 
                             && <div className="featured-info">
                                 <motion.p initial={{opacity: 0}} animate={{opacity: 1}}>{item.title} Chair</motion.p>
                                 <motion.p initial={{opacity: 0}} animate={{opacity: 1}}>{item.price}€</motion.p>
                                 </div>}
-                        </motion.div>
-                    }  
+                        </motion.div> 
                     </div>  
-                ))}
+                )})}
             </motion.div>
         </div>
     )
