51c14b32c40f8f0c60e41786f0ef3ea1b2167160
maxim <maximcarlos@gmx.de>
2022-11-04 11:36:50 +0100

    chair details styling and bug fixes

--- src/App.js
+++ src/App.js
@@ -15,43 +15,43 @@ function App() {
   const [Basket, setBasket] = useState([{title: "Hektor", description:"Tub Office Chair, Tan & Black", price: 230, category: "office", wishlist: false, id: 3}])
   const [wishlist, setWishlist] = useState([{title: "Kubrick", description:"Wing Back Chair, Scuba Blue Fabric", price: 525, category: "bedroom", id: 4}])
   const [chairInfo, setChairInfo] = useState([{
-    title: "ch20", thumbnailimg: "thumbnail1", description:"Lorem ipsum dolor sit amet, consetetur", price: 220.00, category: "dining", wishlist: false, id: 1, featured: true, availability: 1, discount: 20,
+    title: "ch20", thumbnailimg: "thumbnail1", description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 220.00, category: "dining", wishlist: false, id: 1, featured: true, availability: 1, discount: 20,
 },
 {
-    title: "ch23", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur", price: 350.00, category: "dining", wishlist: false, id: 2, featured: false, availability: 2, discount: 20
+    title: "ch23", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 350.00, category: "dining", wishlist: false, id: 2, featured: false, availability: 2, discount: 20
 },
 {
-    title: "ch24", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur", price: 230.00, category: "bar", wishlist: false, id: 3, featured: true, availability: 8, discount: 0
+    title: "ch24", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 230.00, category: "bar", wishlist: false, id: 3, featured: true, availability: 8, discount: 0
 },
 {
-    title: "ch26", thumbnailimg: "thumbnail4",description:"Lorem ipsum dolor sit amet, consetetur", price: 525.00, category: "living", wishlist: false, id: 4, featured: false, availability: 9, discount: 30
+    title: "ch26", thumbnailimg: "thumbnail4",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 525.00, category: "living", wishlist: false, id: 4, featured: false, availability: 9, discount: 30
 },
 {
-    title: "ch29", thumbnailimg: "thumbnail5",description:"Lorem ipsum dolor sit amet, consetetur", price: 230.00, category: "bar", wishlist: false, id: 5, featured: false, availability: 8, discount: 0
+    title: "ch29", thumbnailimg: "thumbnail5",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 230.00, category: "bar", wishlist: false, id: 5, featured: false, availability: 8, discount: 0
 },
 {
-    title: "ch30", thumbnailimg: "thumbnail1",description:"Lorem ipsum dolor sit amet, consetetur", price: 300.00, category: "dining", wishlist: false, id: 6, featured: true, availability: 3, discount: 0
+    title: "ch30", thumbnailimg: "thumbnail1",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 300.00, category: "dining", wishlist: false, id: 6, featured: true, availability: 3, discount: 0
 },
 {
-    title: "ch33", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur", price: 250.00, category: "living", wishlist: false, id: 7, featured: false, availability: 5, discount: 0
+    title: "ch33", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 250.00, category: "living", wishlist: false, id: 7, featured: false, availability: 5, discount: 0
 },
 {
-    title: "ch36", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur", price: 460.00, category: "living", wishlist: false, id: 8, featured: true, availability: 2, discount: 0
+    title: "ch36", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 460.00, category: "living", wishlist: false, id: 8, featured: true, availability: 2, discount: 0
 },
 {
-    title: "ch37", thumbnailimg: "thumbnail4",description:"Lorem ipsum dolor sit amet, consetetur", price: 400.00, category: "dininig", wishlist: false, id: 9, featured: false, availability: 8, discount: 0
+    title: "ch37", thumbnailimg: "thumbnail4",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 400.00, category: "dininig", wishlist: false, id: 9, featured: false, availability: 8, discount: 0
 },
 {
-    title: "ch46", thumbnailimg: "thumbnail5",description:"Lorem ipsum dolor sit amet, consetetur", price: 500.00, category: "dininig", wishlist: false, id: 10, featured: true, availability: 8, discount: 30
+    title: "ch46", thumbnailimg: "thumbnail5",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 500.00, category: "dininig", wishlist: false, id: 10, featured: true, availability: 8, discount: 30
 },
 {
-    title: "ch47", thumbnailimg: "thumbnail1",description:"Lorem ipsum dolor sit amet, consetetur", price: 525.00, category: "living", wishlist: false, id: 11, featured: false, availability: 7, discount: 20
+    title: "ch47", thumbnailimg: "thumbnail1",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 525.00, category: "living", wishlist: false, id: 11, featured: false, availability: 7, discount: 20
 },
 {
-    title: "ch88b", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur", price: 390.00, category: "bar", wishlist: false, id: 12, featured: false, availability: 2, discount: 0
+    title: "ch88b", thumbnailimg: "thumbnail2",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 390.00, category: "bar", wishlist: false, id: 12, featured: false, availability: 2, discount: 0
 },
 {
-    title: "ch88t", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur", price: 320.00, category: "bar", wishlist: false, id: 13, featured: false, availability: 5, discount: 0
+    title: "ch88t", thumbnailimg: "thumbnail3",description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum", price: 320.00, category: "bar", wishlist: false, id: 13, featured: false, availability: 5, discount: 0
 }])
 
   return (
--- src/chairDetails.js
+++ src/chairDetails.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useLocation, useParams } from "react-router-dom"
 import { WishlistManage } from "./components/wishlistManage"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export function ChairDetails({chairInfo, setChairInfo, wishlist, setWishlist, setBasket, Basket}) {
     const location = useLocation()
@@ -21,30 +22,68 @@ export function ChairDetails({chairInfo, setChairInfo, wishlist, setWishlist, se
     }
 
     return(
-        <div>
-            <div>
+        <div >
+            <div className="info-container">
+                <div className="image-container">
                     <img src={`/images/${chair.title}.png`}  alt={chair.title} className="detailsImage"></img>
+                </div>
+                <div className="chair-info">
                     <h1>{chair.title} | by Lorem Ipsum</h1>
                     {chair.availability <= 4 
-                    ? <p style={{color: "#c79393"}}>Availability: only {chair.availability} available!</p>
-                    : <p>Availability: {chair.availability} in Stock</p>}
-                    <p>{chair.description}</p>
+                    ? <p className="chair-availability" style={{color: "#c79393"}}>Availability: only {chair.availability} available!</p>
+                    : <p className="chair-availability">Availability: {chair.availability} in Stock</p>}
+                    <p className="chair-description">{chair.description}</p>
                     {chair.discount > 0
                     ? <div className="priceDiscounted">
-                        <p className="discountPrice">{getSalePrice(chair.price, chair.discount)}€</p>
-                        <p className="originalPrice">{chair.price}€</p>
+                        <p className="originalPrice">{chair.price}.00 €</p>
+                        <p className="discountPrice">{getSalePrice(chair.price, chair.discount)}.00 €</p>
                     </div>
-                    : <p className="originalPrice">{chair.price}</p>}
+                    : <p className="no-discount">{chair.price}€</p>}
                     <div className="quantityManager">
-                        <button  onClick={() => {if(quantity > 0) setQuantity(quantity - 1)}}>-</button>
+                        <p>Quantity: </p>
+                        <button  onClick={() => {if(quantity > 1) setQuantity(quantity - 1)}}>-</button>
                         <p>{quantity}</p>
                         <button onClick={() => setQuantity(quantity + 1)}>+</button>
                     </div>
                     {Basket.length === 0 
-                    ? <button onClick={addToBasket}>add to Basket</button>
-                    : <button onClick={addToBasket}>add another to Basket</button>}
-                    
+                    ? <button className="add-to-basket" onClick={addToBasket}>add to Basket</button>
+                    : <button className="add-to-basket" onClick={addToBasket}>add another to Basket</button>}
                 </div>
+            </div>
+            <Tabs className="chair-tabs">
+                <TabList className="itemInfo">
+                    <Tab>Description</Tab>
+                    <Tab>Additional Info</Tab>
+                </TabList>
+                <span></span>
+                <TabPanel className="item-description">
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </TabPanel>
+                <div className="additional-info-container">
+                    <TabPanel className="additional-info">
+                        <div className="info-column">
+                            <p>weight</p>
+                            <p>5kg</p>
+                        </div>
+                        <div className="column-separator"></div>
+                        <div className="info-column">
+                            <p>Size</p>
+                            <p>Medium</p>
+                        </div>
+                        <div className="column-separator"></div>
+                        <div className="info-column">
+                            <p>Color</p>
+                            <p>Brown</p>
+                        </div>
+                        <div className="column-separator"></div>
+                        <div className="info-column">
+                            <p>Materials</p>
+                            <p>Wood</p>
+                        </div>
+                        <div className="column-separator"></div>
+                    </TabPanel>
+                </div>
+            </Tabs>
         </div>
     )
 }
