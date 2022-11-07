b3bb5deb46691b881b8bdc0a322dc9f71f33645b
maxim <maximcarlos@gmx.de>
2022-11-07 19:51:48 +0100

    chairDetails + wishlist responsive

--- src/components/chairDetails.js
+++ src/components/chairDetails.js
@@ -66,10 +66,12 @@ export function ChairDetails({chairInfo, setChairInfo, wishlist, setWishlist, se
                 </TabList>
                 <span></span>
                 <TabPanel className="item-description">
+                    <h3>Description</h3>
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                 </TabPanel>
                 <div className="additional-info-container">
                     <TabPanel className="additional-info">
+                        <h3>Chair Info</h3>
                         <div className="info-column">
                             <p>weight</p>
                             <p>5kg</p>
