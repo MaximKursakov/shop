fffd74bad0d8f6985fa6d3e4401e591fd53098dc
maxim <maximcarlos@gmx.de>
2022-11-14 19:30:40 +0100

    map finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,41 +10,8 @@ export function Contact() {
 
     return(
         <div className="contact-container">
-            <Map
-            initialViewState={{
-                longitude: 11.58598310587922,
-                latitude: 48.16322164738846,
-                zoom: 17,
-            }}
-            mapboxAccessToken={"pk.eyJ1IjoibWF4aW1rdXJzYWtvdiIsImEiOiJjbGFmNWZod2kxMHAxM3BvMmEwbzk1NHhiIn0.gsgJQTXTMGWMQg6f7RvUHg"}
-            style={{width: 600, height: 400}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            >
-            <Marker
-            latitude={48.16322164738846}
-            longitude={11.58598310587922}
-            >
-                <motion.div onClick={() => setMarkerSelected(!markerSelected)} whileHover={{scale: 1.1}} className='map-marker'>
-                    <img src='/images/location-svgrepo-com.svg'></img>
-                </motion.div>
-            </Marker>
-            {markerSelected ? (
-                <div>
-                <Popup
-                onClose={() => setMarkerSelected(!markerSelected)}
-                latitude={48.16322164738847}
-                longitude={11.58598310587923}
-                anchor="right">
-                    <div>
-                        <h3>Location</h3>
-                        <p>4209 Lorem Ipsum</p>
-                        <p>Lorem 202D</p>
-                        <p>Lorem 80201</p>
-                    </div>
-                </Popup>
-                </div>
-            ) : null}
-            </Map>
+            
+            
             <h1>Contact Us</h1>
             <div className="contact-column">
                 <div>
@@ -85,6 +52,45 @@ export function Contact() {
                     <h2>Marketing Inquiries</h2>
                     <p>questions@Lorem.com</p>
                 </div>
+            </div>
+            <div className='map'>
+                <Map 
+                className="map-container"
+                initialViewState={{
+                    longitude: 11.58598310587922,
+                    latitude: 48.16322164738846,
+                    zoom: 17,
+                }}
+                mapboxAccessToken={"pk.eyJ1IjoibWF4aW1rdXJzYWtvdiIsImEiOiJjbGFmNWZod2kxMHAxM3BvMmEwbzk1NHhiIn0.gsgJQTXTMGWMQg6f7RvUHg"}
+                style={{width: "100%", height: "100%"}}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                >
+                <Marker
+                latitude={48.16322164738846}
+                longitude={11.58598310587922}
+                >
+                    <motion.div onClick={() => setMarkerSelected(!markerSelected)} whileHover={{scale: 1.1}} className='map-marker'>
+                        <img src='./images/location-svgrepo-com.svg'></img>
+                    </motion.div>
+                </Marker>
+                {markerSelected ? (
+                    <div>
+                    <Popup
+                    onClose={() => setMarkerSelected(!markerSelected)}
+                    latitude={48.16322164738847}
+                    longitude={11.58598310587923}
+                    anchor="right">
+                        <div>
+                            <h3>Location</h3>
+                            <p>4209 Lorem Ipsum</p>
+                            <p>Lorem 202D</p>
+                            <p>Lorem 80201</p>
+                        </div>
+                    </Popup>
+                    </div>
+                ) : null}
+                <p className='disclaimer'>For development purposes only</p>
+                </Map>
             </div>
         </div>
     )
