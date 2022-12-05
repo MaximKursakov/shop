6b54bec837dfb389ddf873b1f15f289bd7cc8fbd
maxim <maximcarlos@gmx.de>
2022-12-05 18:09:25 +0100

    further responsivness improvements

--- src/components/ChairShop/chairShop.js
+++ src/components/ChairShop/chairShop.js
@@ -9,7 +9,8 @@ import "./ChairShop.css"
 export function ChairShop({displayChairs, chairInfo, setChairInfo, wishlist, setWishlist }) {
     
     const [isHovering, setIsHovering] = useState(null)
-
+    console.log(window.innerWidth)
+    if (window.innerWidth <= 970) console.log("hey")
     return (
         <div>
             <div className="shop-section">
@@ -22,23 +23,32 @@ export function ChairShop({displayChairs, chairInfo, setChairInfo, wishlist, set
                     viewport={{once: true}}
                     className="shop-item" 
                     key={chair.id}>
-                            <motion.div 
-                                style={{
-                                backgroundImage: `url(/shop/images/${chair.thumbnailimg}.jpg)`}}  
-                                alt={chair.title} 
-                                className="chair"
-                                onMouseOver={() => setIsHovering(chair.id)}
-                                onMouseOut={() => setIsHovering(false)}>
-                                    {isHovering === chair.id 
-                                    && <div className="chair-hover">
-                                        <WishlistManage chair={chair} wishlist={wishlist} setWishlist={setWishlist} chairInfo={chairInfo} setChairInfo={setChairInfo}></WishlistManage>
-                                        <div className="chair-mid">
-                                            <p>{chair.title}</p>
-                                            <Link to={`/shop/Chairs/${chair.title}`} state={{chair}}> {">"} </Link>
-                                        </div>
-                                        </div>}
-                                </motion.div>
-                                <p className="media-title">{chair.title}</p>
+                        {window.innerWidth <= 970 ?
+                        <Link 
+                            style={{
+                            backgroundImage: `url(/shop/images/${chair.thumbnailimg}.jpg)`}}  
+                            alt={chair.title} 
+                            className="chair" 
+                            to={`/shop/Chairs/${chair.title}`} state={{chair}}> 
+                        </Link> 
+                            : <motion.div 
+                            style={{
+                            backgroundImage: `url(/shop/images/${chair.thumbnailimg}.jpg)`}}  
+                            alt={chair.title} 
+                            className="chair"
+                            onMouseOver={() => setIsHovering(chair.id)}
+                            onMouseOut={() => setIsHovering(false)}>
+                                {isHovering === chair.id 
+                                && <div className="chair-hover">
+                                    <WishlistManage chair={chair} wishlist={wishlist} setWishlist={setWishlist} chairInfo={chairInfo} setChairInfo={setChairInfo}></WishlistManage>
+                                    <div className="chair-mid">
+                                        <p>{chair.title}</p>
+                                        <Link to={`/shop/Chairs/${chair.title}`} state={{chair}}> {">"} </Link>
+                                    </div>
+                                    </div>}
+                        </motion.div>}
+                            
+                            <p className="media-title">{chair.title}</p>
                     </motion.div>
                 ))}
                 </div>
